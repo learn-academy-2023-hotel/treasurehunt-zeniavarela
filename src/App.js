a4f438b003dd175fe14fdc7ecdc0c9f209c7dc76
zeniavarela <zvarela8@gmail.com>
2023-10-30 11:59:08 -0700

    treasure-bomb

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Square from "./components/Square";
 import "./App.css";
+import { click } from "@testing-library/user-event/dist/click";
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -15,8 +16,29 @@ const App = () => {
     "?",
   ]);
 
+  const [treasureLocation, setTreasureLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
+  const [bombLocation, setBombLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
+
   const handleSquareClick = (clickedSquareIndex) => {
-    alert(clickedSquareIndex);
+    // create var holding copy of current state
+    let updatedBoard = [...board];
+    // set condition for if treasure location is same as clicked square
+    if (clickedSquareIndex === treasureLocation) {
+      // then reassign state value at that index to treasure emoji
+      updatedBoard[clickedSquareIndex] = "🍀";
+      // setBoard(updatedBoard)
+    } else if (clickedSquareIndex === bombLocation) {
+      updatedBoard[clickedSquareIndex] = "🗿";
+      // setBoard(updatedBoard)
+    } else {
+      updatedBoard[clickedSquareIndex] = "☘️";
+      // setBoard(updatedBoard)
+    }
+    setBoard(updatedBoard);
   };
 
   return (
